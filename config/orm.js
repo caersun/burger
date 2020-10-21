8e968fef7142615c82fb845920cb6b6808c6e8b6
Cynthia Garcia <i.cynthiagarcia@gmail.com>
2020-10-21 11:40:48 -0500

    created methods for orm and burger model

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,93 @@
 // Import MySQL connection.
 const connection = require("../config/connection.js");
 
+// Helper function for SQL syntax
+// Takes in a number and returns a string of the number of question marks
+function createValueMarks(num) {
+    let array = [];
+
+    for (let i = 0; i < num; i++) {
+        array.push("?");
+    }
+
+    return array.toString();
+}
+
+// Helper function for SQL syntax
+// Takes in object and converts object's key/value pairs to SQL syntax
+// Doesn't the latest version of MySQL take in objects at ? and translate them on their site? I think I did this for previous hw
+function objectToSQL(object) {
+    const array = [];
+
+    for (let key in object) {
+        let value = object[key]; // e.g. for object: { key: value }
+
+        if (Object.hasOwnProperty.call(object, key)) { // TODO: What is this line doing?
+            if (typeof value === "string" && value.indexOf(" ") >= 0) { // If value is a string and has spaces, then wrap the value in single-quotes
+                value = `'${value}'`;
+            }
+        }
+
+        array.push(`${key}=${value}`);
+    }
+};
+
 // ORM object
 const orm = {
-    selectAll: () => {},
+    selectAll: (tableInput, cb) => {
+        const query = `SELECT * FROM ??`;
 
-    insertOne: () => {},
+        connection.query(query, [tableInput], (err, result) => {
+            if (err) {
+                throw err;
+            }
 
-    updateOne: () => {},
+            cb(result);
+        });
+    },
+
+    insertOne: (table, cols, vals, cb) => {
+        const columns = cols.toString();
+        const questionMarks = createValueMarks(vals.length);
+        const query = `INSERT INTO ${table} (${columns}) VALUES (${questionMarks})`;
+
+        connection.query(query, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        })
+    },
+
+    updateOne: (table, objColVals, condition, cb) => {
+        const changes = objectToSQL(objColVals);
+        const query = `UPDATE ${table} SET ${changes} WHERE ${condition}`;
+
+        console.log(query);
+
+        connection.query(query, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    deleteOne: (table, condition, cb) => {
+        const query =  `DELETE FROM ${table} WHERE ${condition}`;
+
+        console.log(query);
+
+        connection.query(query, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
 }
 
 // Export the orm object
--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,34 @@
 // Import the ORM to create functions that will interact with the database
+// const connection = require("../config/connection.js");
 const orm = require("../config/orm.js");
+
+const tableName = 'burgers';
 
 const burger = {
     // TODO: Create the code that will call the ORM functions using burger specific input for the ORM
+    selectAll: (cb) => {
+        orm.selectAll(tableName, (res) => { // TODO: What is res - a result or response?
+            cb(res); // TODO: Go back to class vid - what are we doing calling res back?
+        });
+    },
+
+    insertOne: (cols, vals, cb) => {
+        orm.insertOne(tableName, cols, vals, (res) => {
+            cb(res);
+        });
+    },
+
+    updateOne: (objColVals, condition, cb) => {
+        orm.updateOne(tableName, objColVals, condition, (res) => {
+            cb(res);
+        });
+    },
+
+    deleteOne: (condition, cb) => {
+        orm.deleteOne(tableName, condition, (res) => {
+            cb(res);
+        });
+    }
 };
 
 // Export the database functions for the controller 
